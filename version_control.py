4003efca5ac3cff04b1cb79695fc05769f48abca
12345divya12345 <21148@students.lynfield.school.nz>
2025-03-13 09:26:13 +1300

    Update version_control.py

--- version_control.py
+++ version_control.py
@@ -23,3 +23,27 @@ weekly_pay = float(hours*rate)
 
 # Printing out the users gross pay
 print("You made $", weekly_pay, "this week :)")
+print("")
+
+print("Your tax can either be 0.17, 0.25, or 0.33")
+
+# Asking the user for their tax rate
+tax_rate = float(input("How much is your tax?: "))
+print("")
+
+# Calculating the tax deduction by multiplying the tax rate by the weekly pay
+tax_deduction = tax_rate*weekly_pay
+
+# Calculating the net income by subtracting the tax deduction from the weekly pay
+net_income = weekly_pay - tax_deduction
+
+# Printing out the Net income for the user
+print("Your Net Income is $", net_income, ":)")
+print("")
+
+# Calculating the yearly income by multiplying the weekly net income by the number of weeks in a year (52)
+yearly_income = net_income*52
+
+# Printing out a message which tells the user their yearly income with tax
+print("You earn $", yearly_income, "yearly (with tax deduced) :)")
+
